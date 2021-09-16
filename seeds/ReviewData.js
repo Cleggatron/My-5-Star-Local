972724a3b724d145e38ae467eefd88e96370eb58
Georgia <ghobs987@gmail.com>
2021-09-16 21:45:19 +0100

    finalised seed data

--- seeds/ReviewData.js
+++ seeds/ReviewData.js
@@ -1,9 +1,21 @@
 const { Review } = require('../models');
 
 const ReviewData = [
- 
+    {
+        text: 'I had the pie, I like pie.',
+        rating: 5,
+        user_id: 1,
+        restaurant_id: 1,
+      },
+
+      {
+        text: 'let them eat cake',
+        rating: 2,
+        user_id: 2,
+        restaurant_id: 2,
+      },
 ];
 
-const seedReview = () => User.bulkCreate(ReviewData);
+const seedReview = () => Review.bulkCreate(ReviewData);
 
 module.exports = seedReview;
